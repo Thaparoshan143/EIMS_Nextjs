823997f75ea4bec19d15b8850726d137d50a96be
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-18 14:43:20 +0545

    add: basic setup in latest app

---
+++ lib/data/_footerItems.tsx
@@ -0,0 +1,29 @@
+export const _footerItems = [
+    {
+        title: "Home",
+        url: "#"
+    },
+    {
+        title: "Sitemap",
+        url: "#"
+    }
+];
+
+export const _socialLinks = [
+    {
+        media: "Facebook",
+        url: "#"
+    },
+    {
+        media: "Instagram",
+        url: "#"
+    },
+    {
+        media: "Linkedin",
+        url: "#"
+    },
+    {
+        media: "Youtube",
+        url: "#"
+    }
+];
---
+++ lib/data/_navItems.tsx
@@ -0,0 +1,14 @@
+export const _navItems = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "Menu",
+        url: "#"
+    },
+    {
+        title: "Login",
+        url: "/login"
+    }
+];
