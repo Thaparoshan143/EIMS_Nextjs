729bbb43493e79866b1b1d5ebbda73767aee02e3
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-24 14:04:17 +0545

    add: cGet & cPost custom function (axios request wrapper)
    prev. cFetch is subsituted by cGet function, but for compatibility, cFetch is forwarded of cGet function

--- lib/utils/fetch.ts
+++ lib/utils/fetch.ts
@@ -1,21 +1,50 @@
+import axios from "axios";
+
+const axiosIns = axios.create({
+    baseURL: '/',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
 /**
- * This is the custom fetch for getting from urls..
+ * Custom GET request wrapper from urls (Axios based)..
+ * args: url
  */
-export const cFetch = async (url: string) => {
+export const cGet = async (url: string) => {
     
     try {
-        // for now using default fetch.. 
-        const res = await fetch(url)
+        const res = await axiosIns.get(url);
 
-        if (!res.ok) {
-            console.error(`Error fetching with given URL: ${url} with status code: ${res.status}` )
-            return null;
-        }
-
-        const data = await res.json();
-
+        const data = await res.data;
         return data;
     } catch (error) {
+        console.error(error);
+        return null;
+    }
+}
+
+/**
+ * for prev. support cFetch simply cGet forwarder..
+ */
+export const cFetch = cGet;
+
+
+/**
+ * Custom POST request wrapper from urls (Axios based)..
+ * args: url, data (post object)
+ */
+export const cPost = async (url: string, data: any) => {
+    
+    console.log("In the post method: rec");
+    console.log(typeof data);
+    console.log(data);
+    try {
+        const res = await axiosIns.post(url, data);
+
+        return await res.data;
+    } catch (error) {
+        console.log("Idk error here in frontend forward");
         console.error(error);
         return null;
     }
