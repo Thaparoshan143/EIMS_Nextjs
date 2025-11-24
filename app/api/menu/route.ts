ef017ce5b1d0ef709c465bc661e13a927b22d190
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-24 14:09:35 +0545

    update: POST endpoint added, query wrapper used for ease
    #note: createTable helper function is also added, which is to be runned only once to generate/create table for different tablename..

--- app/api/menu/route.ts
+++ app/api/menu/route.ts
@@ -1,17 +1,42 @@
-import pool from '@/lib/utils/dbutil';
+import { firstTableConstruct, getLastRecordId, getRecords, insertRecord } from '@/lib/utils/dbquery';
 import { NextRequest, NextResponse } from 'next/server';
  
 export async function GET(req: NextRequest) {
 
     try {
+        // only call for table creation
+        // createTable();
+
         // please ensure that the menu table exists in the database... before select query..
-        const client = await pool.connect();
-        const result = await client.query("SELECT * FROM menu;");
-        client.release();
-        
-        return NextResponse.json(result.rows, { status: 200 });
+        const records = await getRecords('menu');
+
+        return NextResponse.json(records, { status: 200 });
     } catch (error) {
-        console.error("Either pool is unreachable or other get req. error")
+        console.error(error);
         return NextResponse.json(error, { status: 404 });
     }
+}
+
+export async function POST(req: NextRequest) {
+    try {
+        const reqEles = await req.json();
+        const prevId = await getLastRecordId('menu', 'id');
+        const updatedRec = await insertRecord('menu', {id: prevId+1, ...reqEles});
+
+        return NextResponse.json(updatedRec, { status: 200 });
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json(error, { status: 404 });
+    }
+}
+
+const createTable = () => {
+    // only call once for new table cration below codes..
+    firstTableConstruct('menu', `
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                imgPath VARCHAR(500),
+                price DECIMAL(10, 2) CHECK(price > 0),
+                quantity INT CHECK(quantity > 0)
+                `)
 }
