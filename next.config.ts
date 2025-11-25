6a4a63405cf2e3811049e27a9b5b41698c32d4cf
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-11-25 09:41:32 +0545

    add: next config for env variable (for vercel deploy assist)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    NEXT_PUBLIC_ADMIN_CRED_USER: process.env.NEXT_PUBLIC_ADMIN_CRED_USER,
+    NEXT_PUBLIC_ADMIN_CRED_PASSWORD: process.env.NEXT_PUBLIC_ADMIN_CRED_PASSWORD,
+    NEXT_PUBLIC_ADMIN_CRED_TOKEN: process.env.NEXT_PUBLIC_ADMIN_CRED_TOKEN,
+
+    NEXT_PUBLIC_MENU_FETCH_URL: process.env.NEXT_PUBLIC_MENU_FETCH_URL,
+    NEXT_PUBLIC_DB_URL: process.env.NEXT_PUBLIC_DB_URL,
+  }
 };
 
 export default nextConfig;
