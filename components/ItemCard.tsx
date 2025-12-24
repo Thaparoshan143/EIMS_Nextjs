9b9a5fb01c914c67b0f4f3c967fcb2c91fcb6650
Thaparoshan143 <101375593+Thaparoshan143@users.noreply.github.com>
2025-12-24 12:03:48 +0545

    update: minor design change for ItemElements

--- components/ItemCard.tsx
+++ components/ItemCard.tsx
@@ -9,16 +9,18 @@ interface IItemProps extends IMenuItem {
 
 const ItemElements = ( { name, imgpath, price, quantity }: IItemProps ) => {
     return (
-        <div className="flex flex-col justify-evenly items-center min-w-64 min-h-32 gap-2">
+        <div className="flex flex-col justify-between items-center min-w-64 min-h-32 h-full gap-2">
             <h1 className="font-bold text-xl text-theme">{name}</h1>
             <img src={imgpath} alt={name} className="min-w-32 min-h-32 max-w-64 max-h-64"/>
-            <div>
-                <span className="font-semibold">Price: </span>
-                <span>{price}</span>
-            </div>
-            <div>
-                <span className="font-semibold">Quantity: </span>
-                <span>{quantity}</span>
+            <div className='flex flex-col gap-2'>
+                <div>
+                    <span className="font-semibold">Price: </span>
+                    <span>{price}</span>
+                </div>
+                <div>
+                    <span className="font-semibold">Quantity: </span>
+                    <span>{quantity}</span>
+                </div>
             </div>
         </div>
     )
